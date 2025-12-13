527a03408627b1f36d0f0f1a6457fc838a463575
Saadwarraich7223 <saadwarraich7223@gmail.com>
2025-12-13 20:40:26 +0500

    removed cron job for flashsale

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ dotenv.config();
 import app from "./app.js";
 import connectDB from "./src/config/connectDB.js";
 import { configureCloudinary } from "./src/utils/cloudinary.js";
-import { startFlashSaleCron } from "./src/cron/expireFlashSales.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,7 +10,7 @@ const startServer = async () => {
   try {
     await connectDB(); // connect DB first
     configureCloudinary(); // configure cloudinary
-    startFlashSaleCron();
+
     app.listen(PORT, () => {
       console.log(" Server is running on port " + PORT);
     });
